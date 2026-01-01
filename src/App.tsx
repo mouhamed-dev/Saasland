dcb4ce113cd7f7c02bb348b776a5d542705e3c44
Mouhamed Mbaye <mbaye.mouhamed020@gmail.com>
2026-01-01 19:38:05 +0000

    Déploement final

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-export default function App() {
-  return (
-    <div style={{ color: "red", fontSize: "40px" }}>
-      🚀 SaaSland fonctionne !
-    </div>
-  );
-}
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </HashRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
